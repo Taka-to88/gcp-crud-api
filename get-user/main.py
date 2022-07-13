0e3c472222e0c671f5d1f561287f9a6b1ce40f90
TaKATO KONDO <takatokondou@TaKATOnoMacBook-Pro.local>
2022-07-13 10:45:28 +0900

    取得データ変更

--- get-user/main.py
+++ get-user/main.py
@@ -1,16 +1,15 @@
 import json
 import firebase_admin
-from firebase_admin import firestore    
+from firebase_admin import firestore
 
 
 def get_user(request):
     db = firestore.Client()
 
-    docs = db.collection('users').get()
+    docs = db.collection('COMPANY').document('会社1').get()
     users_list = []
     for doc in docs:
         users_list.append(doc.to_dict())
     return_json = json.dumps({"users": users_list}, ensure_ascii=False)
 
     return return_json
-
